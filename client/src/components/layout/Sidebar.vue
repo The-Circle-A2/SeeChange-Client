640a59bc31f1163b58e88a9386de48736c7cfb49
Vincent <vmf.bas@student.avans.nl>
2021-06-15 11:15:10 +0200

    Created dynamic dashboard

--- client/src/components/layout/Sidebar.vue
+++ client/src/components/layout/Sidebar.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="sidebar"></div>
+  <div class="sidebar">
+    <h4 class="header">People you follow</h4>
+    <Following
+      v-for="item in items"
+      :key="item._id"
+      :viewers="item.viewers"
+      :name="item.name"
+      :city="item.city"
+    />
+  </div>
 </template>
 
 <script>
+import Following from "../layout/Following.vue";
+import { mapGetters } from "vuex";
+
 export default {
+  computed: mapGetters({
+    items: "dummy/following",
+  }),
+
+  components: { Following },
   name: "Sidebar",
 };
 </script>
@@ -11,6 +28,7 @@ export default {
 <style lang="scss" scoped>
 .sidebar {
   flex: none;
+  flex-direction: column;
   display: flex;
   box-sizing: border-box;
   height: calc(100vh - 90px);
@@ -19,5 +37,10 @@ export default {
     var(--sidebar-border-color);
   background-color: #fff;
   padding: 40px 30px;
+}
+
+.header {
+  margin: 0px auto 15px auto;
+  height: 1rem;
 }
 </style>
---
+++ client/src/store/modules/dummy.js
@@ -0,0 +1,59 @@
+export const state = {
+  streamlist: [
+    {
+      _id: 1,
+      title: "TEST TEST TEST TEST TEST TEST TEST TEST TEST",
+      name: "John Doe",
+      city: "city",
+    },
+    {
+      _id: 2,
+      title: "Lorum ipsum jayke mofu duval rept wuga...",
+      name: "Ben Dover",
+      city: "city",
+    },
+    {
+      _id: 3,
+      title: "Lorum ipsum jayke mofu duval rept wuga...",
+      name: "Dees Nuts",
+      city: "Kaas",
+    },
+    {
+      _id: 4,
+      title: "Lorum ipsum jayke mofu duval rept wuga...",
+      name: "Erik Jan",
+      city: "city",
+    },
+    {
+      _id: 5,
+      title: "Lorum ipsum jayke mofu duval rept wuga...",
+      name: "Erik Jan",
+      city: "city",
+    },
+  ],
+  following: [
+    {
+      _id: 1,
+      viewers: 1234,
+      name: "John Doe",
+      city: "city",
+    },
+    {
+      _id: 2,
+      viewers: 4321,
+      name: "Ben Dover",
+      city: "city",
+    },
+    {
+      _id: 3,
+      viewers: 2134,
+      name: "Dee Znuts",
+      city: "village",
+    },
+  ],
+};
+
+export const getters = {
+  streams: (state) => state.streamlist,
+  following: (state) => state.following,
+};
