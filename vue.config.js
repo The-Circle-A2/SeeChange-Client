dc755fabc997de7ebe8ee42057efe7e60cc80a25
Bart Fijneman <info@bartfijneman.com>
2021-06-22 13:07:40 +0200

    Fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    outputDir: path.resolve(__dirname, './client/public'),
+    outputDir: path.resolve(__dirname, '../seechange-client.the-circle.designone.nl/public'),
     devServer: {
       proxy: {
           '/api/': {
