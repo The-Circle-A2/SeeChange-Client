72646846b5bebd68f5c3d47733de7473d09b6755
Bart Fijneman <info@bartfijneman.com>
2021-06-24 18:57:02 +0200

    Added connection validation

--- src/pages/authenticate/connect.vue
+++ src/pages/authenticate/connect.vue
@@ -29,6 +29,13 @@
 
 <script>
 import { required } from "vuelidate/lib/validators";
+import axios from 'axios';
+const JSEncrypt = require("jsencrypt/bin/jsencrypt");
+const CryptoJS = require("crypto-js");
+
+const {
+  verifyMessage
+} = require('../../socket/rsaIntegrityHandler');
 
 export default {
   name: "SignIn",
@@ -55,15 +62,70 @@ export default {
 
   methods: {
     connect() {
+      // Bart
+
       this.$v.$touch();
       if (this.$v.$invalid) return;
 
-      this.$store
-        .dispatch("user/connect", this.user)
-        .then(() => {
-          this.$router.push({name: 'dashboard'});
+      axios.get('http://truyou.the-circle.designone.nl/user/' + this.user.username, {
+        headers: {
+          'Content-Type': 'plain/text',
+        },
+        transformResponse: [data => data]
+      })
+        .then((res) => {
+          /*
+          Validate server response
+           */
+          let verified = verifyMessage({
+            message: res.data,
+            timestamp: res.headers['x-timestamp'],
+            signature: res.headers['x-signature']
+          });
+
+          if(!verified) {
+            alert('Pas op! Het verzoek van TruYou is aangepast door hackers.');
+            return;
+          }
+
+          let data = JSON.parse(res.data);
+
+          const sign = new JSEncrypt();
+          sign.setPrivateKey(this.user.private_key);
+          let phrase = sign.sign('phrase', CryptoJS.SHA256, "sha256");
+
+
+          let verifyLocal = new JSEncrypt({default_key_size: 512});
+          verifyLocal.setPublicKey(data.public_key);
+          if(!verifyLocal.verify('phrase', phrase, CryptoJS.SHA256)) {
+            alert('Je username en/of private key zijn niet juist.');
+            return;
+          }
+
+          this.$store
+              .dispatch("user/connect", this.user)
+              .then(() => {
+                this.$router.push({name: 'dashboard'});
+              })
+              .catch(() => {});
         })
-        .catch(() => {});
+        .catch((error) => {
+          /*
+          Validate server response
+           */
+          let verified = verifyMessage({
+            message: error.response.data,
+            timestamp: error.response.headers['x-timestamp'],
+            signature: error.response.headers['x-signature']
+          });
+
+          if(verified) {
+            alert('Je username en/of private key zijn niet juist.');
+          }
+          else {
+            alert('Pas op! De response van TruYou is aangepast door hackers.');
+          }
+        });
     },
   },
 };
